b475e619a7118ce4970f057f08207d573f9701ea
Alexandr Odarchenko <Nolofinve1991@gmail.com>
2023-04-08 13:45:22 +0300

    домашняя работа 4 семинар

---
+++ 1_Задача/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine(ExponentiationAtoB(a, b));
+
+int ExponentiationAtoB(int a, int b)
+{
+    int tmp = a;
+    
+    for(int i = 1; i < b; i++)
+    {
+        a = a * tmp;
+    }
+
+    return a;
+}
---
+++ 2_Задача/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SummNumber(number));
+
+int SummNumber(int number)
+{
+    int [] array = BuildArrayFromNumers(number);
+    int summ = 0;
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        summ += array[i]; 
+    }
+    return summ;
+}
+
+int[] BuildArrayFromNumers(int number)
+{
+    int length = SizeNubmer(number);
+
+    int[] array = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = number % 10;
+        number = number / 10;
+    }
+    return array;
+}
+
+int SizeNubmer(int number)
+{
+    int i = 0;
+
+    while (number != 0)
+    {
+        number /= 10;
+        i++;
+    }
+
+    return i;
+}
